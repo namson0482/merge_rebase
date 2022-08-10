bd830ed2935c15c571a8b23f8e7de7902d796e8a
Son Vu <son.vu@smartbear.com>
2022-08-10 10:10:07 +0700

    add a new LibraryOne.java

---
+++ src/main/java/vunam/libraries/LibraryOne.java
@@ -0,0 +1,13 @@
+package vunam.libraries;
+
+public class LibraryOne {
+
+    String name;
+
+    String version;
+
+    public LibraryOne(String name, String version) {
+        this.name = name;
+        this.version = version;
+    }
+}
